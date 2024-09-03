dd666b0817dfe6c7c319f5f9abb06e1dbc7a8d44
KaffinPX <KaffinPX@gmail.com>
2024-09-03 05:48:32 +0300

    Cleanup (1/2)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react() ],
-  base: "/kaspian-krc20/",
+  base: "/Kaspian-KRC20/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
