3ed7ebe3dd3ee13c76496c9524f1512512bd1dad
KaffinPX <KaffinPX@gmail.com>
2024-09-03 04:56:50 +0300

    Automatically build and upload to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react() ],
+  base: "/Kaspian-KRC20/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
